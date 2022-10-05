9420405a0b77beb7d6604a96f4f970c2eabd5b7c
Faith Oyedemi <faithoyedemi@gmail.com>
2022-10-05 13:45:04 -0400

    modify binary entrypoint to use lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+//! 
+use newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind to address");
+    run(listener)?.await
 }
+
+
