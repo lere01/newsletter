6a46263e91e08862db59e9997c49596071b6ba9e
Faith Oyedemi <faithoyedemi@gmail.com>
2022-10-05 13:44:37 -0400

    health_check integration test

---
+++ tests/health_check.rs
@@ -0,0 +1,30 @@
+//! tests/health_check.rs
+//! 
+//! 
+use std::net::TcpListener;
+
+
+#[tokio::test]
+async fn health_check_works() {
+    let base_url = spawn_app();
+    let client = reqwest::Client::new();
+    let response = 
+        client
+        .get(&format!("{}/health_check", &base_url))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    // assert_eq!(Some(0), response.content_length());
+}
+
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
