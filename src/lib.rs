7c71de63e10647762e9a421a89729ce466bc95f2
Faith Oyedemi <faithoyedemi@gmail.com>
2022-10-05 13:44:07 -0400

    introduce library in project

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+//! lib.rs
+//! 
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+// Mark `run` as public to it can be made available in our binary. Also, it is not a binary entrypoint
+// so we can mark it as async
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(
+        ||
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+        )
+        .listen(listener)?
+        .run();
+    
+        Ok(server)
+}
