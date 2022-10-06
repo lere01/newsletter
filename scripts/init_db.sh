25ee3b74d91afb9f11b7deb5e0c2cbde5ad5fd73
Faith Oyedemi <faithoyedemi@gmail.com>
2022-10-06 16:35:35 -0400

    update db script to wait until database is ready

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,3 +14,16 @@ docker run \
 -p "${DB_PORT}":5432 \
 -d postgres \
 postgres -N 1000
+
+until 
+    PGPASSWORD="${DB_PASSWORD}" psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; 
+do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@$localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
