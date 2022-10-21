e3f2be445cde449e44919b4e610bae2547b4dcef
Faith Oyedemi <faithoyedemi@gmail.com>
2022-10-20 23:22:47 -0400

    prepare for cloud deployment

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,49 +1,115 @@
+//! src/configuration.rs
+//! 
 use secrecy::{ExposeSecret, Secret};
+use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgSslMode;
+use sqlx::ConnectOptions;
+use serde_aux::field_attributes::deserialize_number_from_string;
+
+
+pub enum Environment {
+    Local,
+    Production
+}
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+}
+
+
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(
+                format!(
+                    "{} is not a supported environment. Use either `local` or `production`.",
+                    other
+                )
+            ),
+        }
+    }
+}
+
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn connection_string(&self) -> PgConnectOptions {
+        let mut options = self.connection_string_without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn connection_string_without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 }
 
+
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // set common configuration path
+    let base_path = std::env::current_dir().expect("Failed to determine the current dirrectory");
+    let config_dir = base_path.join("configuration");
+
+    // check for running environment and default to local if not specified
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    let env_file = format!("{}.yaml", environment.as_str());
+    
+    // initialize the configuration reader
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
-        ))
+        // the source is coming from a configuration file - configuration.yaml
+        .add_source(config::File::from(config_dir.join("base.yaml")))
+        .add_source(config::File::from(config_dir.join(&env_file)))
+        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
         .build()?;
+    
     settings.try_deserialize::<Settings>()
 }
